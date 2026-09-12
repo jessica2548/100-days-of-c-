9d51a4f26e30db4eb8c5a48f5898bf6496371029
jessica2548 <jessica.33597@stu.upes.ac.in>
2026-09-12 13:06:18 +0530

    Implement simple and compound interest calculations

---
+++ day5.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float p, r, t, si, ci;
+
+    scanf("%f %f %f", &p, &r, &t);
+
+    si = (p * r * t) / 100;
+    ci = p * (pow(1 + r / 100, t) - 1);
+
+    printf("Simple Interest=%.2f, Compound Interest=%.2f", si, ci);
+
+    return 0;
+}
