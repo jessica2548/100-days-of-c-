c46d5b1c125a5854c68a256c01fe6009c06c19e4
jessica2548 <jessica.33597@stu.upes.ac.in>
2026-09-12 13:12:39 +0530

    Add leap year check in day7.1.c

---
+++ day7.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        printf("Year is a leap year");
+    else
+        printf("Year is not a leap year");
+
+    return 0;
+}
