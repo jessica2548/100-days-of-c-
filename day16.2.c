c90946178482f1b09ff396aaa7beb176eb67cbef
jessica2548 <jessica.33597@stu.upes.ac.in>
2026-09-14 22:49:16 +0530

    Add palindrome check program in day16.2.c

---
+++ day16.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, rev = 0, digit;
+
+    scanf("%d", &n);
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n /= 10;
+    }
+
+    if (original == rev)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
