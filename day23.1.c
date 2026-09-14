46c1db2f16bdc84164f5dd13ca6efce0e811840b
jessica2548 <jessica.33597@stu.upes.ac.in>
2026-09-14 23:12:58 +0530

    Add program to compute approximate sum
    Implement a program to calculate an approximate sum based on user input.

---
+++ day23.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    double sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+        sum += (double)(2 * i) / (4 * i - 1);
+
+    printf("Approximate sum: %.2f", sum);
+
+    return 0;
+}
