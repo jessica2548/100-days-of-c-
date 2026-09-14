faaa38048d274edbb9a552ccab86f29cace0e828
jessica2548 <jessica.33597@stu.upes.ac.in>
2026-09-14 23:03:22 +0530

    Implement binary digit inversion in day20.2.c

---
+++ day20.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, result = 0, place = 1;
+
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+
+        if (digit == 0)
+            digit = 1;
+        else
+            digit = 0;
+
+        result += digit * place;
+        place *= 10;
+        n /= 10;
+    }
+
+    printf("%04d", result);
+
+    return 0;
+}
